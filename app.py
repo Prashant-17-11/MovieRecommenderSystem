6b2b91fa1b2c13cfb7079af7f5825a3b3e08dc7f
Prashant Singh <prashantsingh.999000@gmail.com>
2022-12-08 20:23:59 +0530

    made changes to UI

--- app.py
+++ app.py
@@ -1,64 +1,81 @@
-import pandas as pd
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
 
-def get_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-U'.format(movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def fetch_poster(movie_id):
+    url = 'https://api.themoviedb.org/3/movie/{}?api_key=ff908b9511e6589713db6bf7d3093231&language=en-US'.format(
+        movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
 
 def recommend(movie):
-    movie_index = movies[ movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:7]
-
-    recommend_movies=[]
-    recommend_poster = []
-    for i in movies_list:
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[0:30]:
+        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommend_movies.append(movies.iloc[i[0]].title)
-        # movie poster from api
-        recommend_poster.append(get_poster(movie_id))
-    return recommend_movies,recommend_poster
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-movies_dict = pickle.load(open('movies.pkl','rb'))
+    return recommended_movie_names, recommended_movie_posters
+
+
+def make_grid(cols, rows):
+    grid = [0] * cols
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
+
+
+def show_recommendations(selected_movie):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    for i in range(0, 30, 3):
+        recommended_movie_name_c1 = recommended_movie_names[i]
+        recommended_movie_poster_c1 = recommended_movie_posters[i]
+        recommended_movie_name_c2 = recommended_movie_names[i + 1]
+        recommended_movie_poster_c2 = recommended_movie_posters[i + 1]
+        recommended_movie_name_c3 = recommended_movie_names[i + 2]
+        recommended_movie_poster_c3 = recommended_movie_posters[i + 2]
+
+        grid_index = int(i / 3)
+        my_grid[grid_index][0].write(recommended_movie_name_c1)
+        my_grid[grid_index][0].image(recommended_movie_poster_c1)
+        my_grid[grid_index][1].write(recommended_movie_name_c2)
+        my_grid[grid_index][1].image(recommended_movie_poster_c2)
+        my_grid[grid_index][2].write(recommended_movie_name_c3)
+        my_grid[grid_index][2].image(recommended_movie_poster_c3)
+
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-st.title("Movie Recommender System")
-
-selected_movie_name = st.selectbox(
-'Enter movies name',
-(movies['title'].values)
+st.title('Movie Recommender System')
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    'How would you like to be contacted?',
+    movie_list
 )
+if st.button('Show Recommendation'):
+    my_grid = make_grid(10, (3, 3, 3))
+    show_recommendations(selected_movie)
 
-if st.button('Search'):
-    names,poster = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5, col6 = st.columns(6)
 
-    with col1:
-        st.text(names[0])
-        st.image(poster[0])
-
-    with col2:
-        st.text(names[1])
-        st.image(poster[1])
-
-    with col3:
-        st.text(names[2])
-        st.image(poster[2])
-
-    with col4:
-        st.text(names[3])
-        st.image(poster[3])
-
-    with col5:
-        st.text(names[4])
-        st.image(poster[4])
-
-    with col6:
-        st.text(names[5])
-        st.image(poster[5])
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
+st.markdown(hide_img_fs, unsafe_allow_html=True)
